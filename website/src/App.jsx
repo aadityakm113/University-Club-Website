8cfd0327372fc50329e60a67d95796b360e22865
aadityakm113 <aadityakm113@gmail.com>
2024-05-25 21:48:06 +0530

    first-commit

---
+++ website/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Body from './components/body/Body'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+    <div className='page'>
+      <Navbar />
+      <Body />
+    </div>
+  )
+}
+
+export default App
---
+++ website/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+const Navbar = () => {
+  return (
+    <div className='navbar_container'>
+      <h1>Clubs of Gitam</h1>
+      <h1>Upcoming Events</h1>
+      <h1>Sign in</h1>
+    </div>
+  )
+}
+
+export default Navbar
