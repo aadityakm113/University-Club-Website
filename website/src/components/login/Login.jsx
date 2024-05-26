1e56f82d542e8e8573eacde2e42931716c552a74
Lalit Narayanam <narayanamlalit@gmail.com>
2024-05-26 16:45:06 +0530

    styling

--- website/src/components/login/Login.jsx
+++ website/src/components/login/Login.jsx
@@ -11,11 +11,16 @@ const Login = () => {
                 <p>Welcome to the official events</p>
                 <p>page of GITAM UNIVERSITY</p>
             </div>
+            <div className='line'>
+              <p>Manage your events anywhere and</p>
+              <p>anytime.</p>
+            </div>
         </div>
+        <div className='vertical'></div>
         <form className='Sign-in'>
 
         <div className='user'>
-        <input type="text" id="username" className='textbox' placeholder="Username"/>
+        <input type="text" id="username" className='textbox' placeholder="Email"/>
         <input type="password" id="password" className='textbox' placeholder="Password"/>
         <div className='Sign-in'>
         <button id="Sign in" className='button'>Sign in</button>
