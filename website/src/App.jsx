ea8dcf79904bfd987e0d7420c8d080afcf88cf51
Lalit Narayanam <narayanamlalit@gmail.com>
2024-05-26 18:53:11 +0530

    guidelines

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -4,6 +4,7 @@ import Events from './components/events/Events'
 import Home from './components/home/Home'
 import Navbar from './components/navbar/Navbar'
 import Login from './components/login/Login'
+import Guidelines from './components/guidelines/Guidelines'
 import {BrowserRouter as Router ,Route,Routes} from 'react-router-dom'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/clubs' element={<Clubs />} />
           <Route path='/events' element={<Events />} />
           <Route path='/log-in' element={<Login />} />
+          <Route path='/guidelines' element={<Guidelines />}/>
         </Routes>
       </div>
     </Router>
