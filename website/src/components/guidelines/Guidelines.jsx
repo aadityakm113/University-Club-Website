36a002ad75271aa4601bffa5a644fcd83c996669
aadityakm113 <aadityakm113@gmail.com>
2024-05-29 12:49:23 +0530

    altered-popup

--- website/src/components/guidelines/Guidelines.jsx
+++ website/src/components/guidelines/Guidelines.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import './guidelines.css'
+import React from "react";
+import "./guidelines.css";
 const Guidelines = () => {
   return (
-    <div className='guidelines'>
+    <div className="guidelines">
       <h3>DUTIES OF THE CORE BODY:</h3>
-      <p>Hold the minimum of 1 meeting for the general membership per semester.Coordinating to plan every event.Assessing the motion for removal or recruitment of a member.May be made Coordinators of different verticals or departments of the community, considering their contribution.</p>
+      <p>
+        Hold the minimum of 1 meeting for the general membership per
+        semester.Coordinating to plan every event.Assessing the motion for
+        removal or recruitment of a member.May be made Coordinators of different
+        verticals or departments of the community, considering their
+        contribution.
+      </p>
       <h3>CHAIR:</h3>
-      <p>Supervising all student organization meetings.Overseeing the entire process for events, projects, and collaborations.Submitting a semester and annual report of student organization activities.Ensuring all content, permission letters, and documents are furnished by the board members responsible are on time and approved.Managing the emails the student organization receives and redirecting them to other officers.Guiding the executives-in-charge of the department.</p>
+      <p>
+        Supervising all student organization meetings.Overseeing the entire
+        process for events, projects, and collaborations.Submitting a semester
+        and annual report of student organization activities.Ensuring all
+        content, permission letters, and documents are furnished by the board
+        members responsible are on time and approved.Managing the emails the
+        student organization receives and redirecting them to other
+        officers.Guiding the executives-in-charge of the department.
+      </p>
       <h3>FINANCE</h3>
-      <p>Managing the funds of the organisation.Submitting invoices for every purchase.Furnishing details of any transaction when asked. Ensuring appropriate prizes go out to winners of each competition. Managing the budget for each event.</p>
+      <p>
+        Managing the funds of the organisation.Submitting invoices for every
+        purchase.Furnishing details of any transaction when asked. Ensuring
+        appropriate prizes go out to winners of each competition. Managing the
+        budget for each event.
+      </p>
       <h3>HUMAN RESOURCES</h3>
-      <p>Recruiting new members into the club.Resolving any grievances or complaints of the members of the organization.Maintaining confidentiality for every member of the organisation.Maintaining a current list of residences,Maintaining and periodically updating a database of alumni and current members of the organization, including:Email addresses PIN/registration numbers Phone numbers Current city of residence Guiding the executives-in-charge of the department.</p>
+      <p>
+        Recruiting new members into the club.Resolving any grievances or
+        complaints of the members of the organization.Maintaining
+        confidentiality for every member of the organisation.Maintaining a
+        current list of residences,Maintaining and periodically updating a
+        database of alumni and current members of the organization,
+        including:Email addresses PIN/registration numbers Phone numbers Current
+        city of residence Guiding the executives-in-charge of the department.
+      </p>
       <h3>SOCIAL MEDIA</h3>
-      <p>Publicity of any event.Updating social media pages of the organisation regularly with all the happenings of the organisation.Maintaining a strong persona on, and managing social media.</p>
-      <div className='contact'>
-        For further details please contact studentlife@gitam.edu   08455-221200
+      <p>
+        Publicity of any event.Updating social media pages of the organisation
+        regularly with all the happenings of the organisation.Maintaining a
+        strong persona on, and managing social media.
+      </p>
+      <div className="contact">
+        For further details please contact studentlife@gitam.edu 08455-221200
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Guidelines
+export default Guidelines;
--- website/src/components/popup/Popup.jsx
+++ website/src/components/popup/Popup.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './popup.css';
+import React from "react";
+import "./popup.css";
 
-const Popup = ({ onClose, card }) => {
+const Popup = ({ onClose, data, type }) => {
   return (
     <div className="popup-container">
       <div className="popup-box">
-        <img src={`/assets/${card.image}`} alt={card.title} />
-        <h2>{card.title}</h2>
-        <p>{card.description}</p>
+        {type === "home" ? (
+          <div>
+            <img src={`/assets/${data.image}`} alt={data.title} />
+            <h2>{data.title}</h2>
+            <p>{data.description}</p>
+          </div>
+        ) : (
+          <div>
+            <img className="ev" src={`/assets/${data.logo}`} alt={data.title} />
+            <h2>{data.title}</h2>
+            <p>{data.description}</p>
+            <p>
+              {data.date} | {data.time}
+            </p>
+          </div>
+        )}
         <button onClick={onClose}>Close</button>
       </div>
     </div>
